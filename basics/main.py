f1728339d5e8bfb850034fd1307293b52e8ff2ba
DIVERMA2501 <divyayushv20@gmail.com>
2025-03-06 16:23:07 +0530

    init

---
+++ basics/main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+# driver.get(f"https://www.amazon.in/s?k=macbook&crid=OMT4TP52VB7&sprefix=macboo%2Caps%2C706&ref=nb_sb_noss_2")
+driver.get("https://fm99.lt/top-10/")
+
+# elem = driver.find_element(By.CLASS_NAME, "puisg-row")
+elem = driver.find_element(By.CLASS_NAME, "top-table")
+print(elem.text)
+
+print(driver.title)
+
+time.sleep(1)
+driver.close()
